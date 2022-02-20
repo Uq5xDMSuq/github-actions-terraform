e122bc72d81c1ade5f0e13271f06c782c4e8a368
Uq5xDMSuq <31223776+Uq5xDMSuq@users.noreply.github.com>
2022-02-21 10:17:04 +1100

    Add Terraform backend

--- main.tf
+++ main.tf
@@ -2,6 +2,22 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state.uq5xdmsuq"
+    key            = "github-actions-demo.terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+  }
+
+  required_providers {
+    aws = {
+      version = "~> 4"
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
 resource "aws_sqs_queue" "terraform_queue" {
   name                      = "terraform-example-queue"
   delay_seconds             = 30
