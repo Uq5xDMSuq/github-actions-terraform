4653c9df3443bd19ad8965cf1797ffdb6a1a5b46
Uq5xDMSuq <31223776+Uq5xDMSuq@users.noreply.github.com>
2022-02-21 10:07:30 +1100

    Decrease SQS delay seconds

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_sqs_queue" "terraform_queue" {
   name                      = "terraform-example-queue"
-  delay_seconds             = 90
+  delay_seconds             = 30
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
